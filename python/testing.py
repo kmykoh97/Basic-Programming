da7814d17b69d2a3f1dfe6390ac0c88fe59dc93f
Koh Meng Yit <kmykoh97@gmail.com>
2018-04-04 17:48:25 -0700

    updated some files

--- python/testing.py
+++ python/testing.py
@@ -1,26 +1,27 @@
-# n = input("please insert a number:")
-# for i in range(n):
-#   print "hello world x%d" %(n) # print "hello world" n times
-#
-# y = 0
-# while y <= 10:
-#   y += 1
-# print y
-
-# listA = (1,2,"happy")
-# print listA
-
-# list1 = [1, 2, "third", 4, 5, 6, 7, 8, 9, 10]
-# print len(list1) # output 3
-# print list1[1] # output 2
-# print list1[:-1:2] # output
-# print list1[-1]
 
 
-# name = input('enter: ')
-# handle = open(name, 'r')
-# counts = dict()
+def main():
+    dilemmaMatrix = [[-1,-1], [-3,0], [0,-3], [-2,-2]]
 
-# for line in handle:
+    ans1 = input("A's strategy? ")
+    ans2 = input("B's strategy? ")
 
-  
+    if (ans1 == "up" and ans2 == "left"):
+        y = 0
+    elif (ans1 == "up" and ans2 == "right"):
+        y = 1
+    elif (ans1 == "down" and ans2 == "left"):
+        y = 2
+    elif (ans1 == "down" and ans2 == "right"):
+        y = 3
+    else:
+        print("Please enter again.")
+        y = "example"
+
+    if (y == 0 or y == 1 or y == 2 or y == 3):
+        print("A's payoff: " + str(dilemmaMatrix[y][0]))
+        print("B's payoff: " + str(dilemmaMatrix[y][1]))
+
+    input()
+
+main()
